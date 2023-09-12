44a965f3be0002a6e3c28a6eaa763283d2573470
wanjala Stephen <stephenwanjala145@gmail.com>
2023-09-12 19:11:18 +0300

    Theme Resources &&  update Agp to 8.3.0-alpha03

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,7 +100,6 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.48")
     ksp ("androidx.hilt:hilt-compiler:1.0.0")
 
-
     implementation ("com.google.accompanist:accompanist-permissions:0.30.1")
 
 
