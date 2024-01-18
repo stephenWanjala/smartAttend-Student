ab2c24f142d5e04ab4402de65995fb32db0b3dd1
wanjala stephen <stephenwanjala145@gmail.com>
2024-01-18 14:43:38 +0300

    - Update hilt Classpath to 2.49 - Upgrade Agp to 1.9.2 and compose CompilerExtensionVersion to 1.5.8 - Enable Edge To Edge upgrade gradle to 8.4

--- app/src/main/java/com/github/stephenwanjala/smartattend/MainActivity.kt
+++ app/src/main/java/com/github/stephenwanjala/smartattend/MainActivity.kt
@@ -3,11 +3,15 @@ package com.github.stephenwanjala.smartattend
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import com.github.stephenwanjala.smartattend.ui.theme.SmartAttendTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +20,13 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.Transparent.toArgb(),
+                Color.Transparent.toArgb(),
+
+                )
+        )
         setContent {
             SmartAttendTheme {
                 // A surface container using the 'background' color from the theme
